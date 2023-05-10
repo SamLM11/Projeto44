b1fc38e5701207458820445f0229605674730a0b
SamLM11 <samuellm2011@gmail.com>
2023-05-10 17:00:44 -0300

    atualização dos js

--- seta.js
+++ seta.js
@@ -15,13 +15,12 @@ class Seta {
       if (keyIsDown(DOWN_ARROW) && this.angle>-30 ) {
         this.angle -= 1;
       }
-  
+
   
       push();
       translate(this.x, this.y);
       rotate(this.angle);
       imageMode(CENTER);
-      image(this.cannon_image, 0, 0, this.width, this.height);
       pop();
       noFill();
     }
